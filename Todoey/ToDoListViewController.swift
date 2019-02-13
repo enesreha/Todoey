7fe338940cb9fd7ed3eba0b2dcbcc4d8d80e9127
enesreha <enesreha@gmail.com>
2019-02-13 18:14:59 +0300

    ToDoList complete with data source and delegate methods

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Enes Reha GILBAZ on 2/12/19.
+//  Copyright © 2019 Enes Reha GILBAZ. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["Call the doctor", "Buy green tea", "Work for 5 hours"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+    //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+  
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      //  let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "ToDoItemCell")//we don't use this one because it will be a problem if we have let's say more than 10 cells. we need reuseable cells. otherwise our app will crash on memory pressure very fast.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+    //MARK: - Tableview Delegate Methods
+    //That method detects which row was selected
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      //  print(itemArray[indexPath.row])
+        
+        //We want to give a checkmark as an accessory to our cell when we click on it
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {// cellForRow(at: indexPath) grabbes a reference to the cell that is at a particular index path. This line of code means that the cell at this indexpath in our tableview is going to have an accessory type of checkmark. Here we say add a checkmark whenever a cell has been selected
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)//when we select a row it turns gray and stays like that. we don't want this. we want it to flash gray and than disappear
+        
+    }
+}
+
